bafabbf0ab1e24b959cc5f71a40fa6035c35800b
Julian Tao <julian.tao@asu.edu>
2022-01-28 10:06:56 -0700

    Improve bash file style

--- mychgpu.sh
+++ mychgpu.sh
@@ -3,7 +3,7 @@
 # This is an example bash file for build and run ChronoGpu projects
 # Update the numbers of compute nodes, cpu cores and gpus (-N, -n, --gres=gpu:) and computing wall time (-t) based on your computation needs
 # Update your email address (--mail-user=) to recieve notifications
-# replace all the instances of "mychgpu" with your project title (such as "sample_generation"
+# replace all the instances of "mychgpu" with your project title (such as "sample_generation")
 
 
 #SBATCH -N 1                        	# number of compute nodes
@@ -20,15 +20,22 @@
 #SBATCH --mail-user=jtao25@asu.edu 	  # send-to email address, change to your own when using
 module purge
 module load eigen/3.3.7-openmpi-3.0.3-gcc-7x cuda/11.2.0 blaze/3.7 cmake/3.20.3 chrono/6.0.0 anaconda/py3 rclone/1.43
-nvidia-smi 				# Useful for seeing GPU status and activity 
-mkdir build 				# Make a new directory to host all cmake files 
-cd build				# Go to the new build directory
-cmake ..				# Generate Makefiles
-make					# Build the project
+nvidia-smi  # Useful for seeing GPU status and activity
+mkdir build # Make a new directory to host all cmake files
+cd build    # Go to the new build directory
+cmake ..    # Generate Makefiles
+make        # Build the project
 srun --mpi=pmi2 ./mychgpu mychgpu.json			# Run the executive
-cd ..					# Return to the project directory
-tar czvf mychgpu_output.tgz ./OUT	# Make a tarball of the output files with compression
+
+########################NOTE########################
+# The following is for data archiving purposes     #
+# If you have not setup rclone ang GoogleDrive yet,# 
+# you may want to remove the following lines       #
+####################################################
+
+cd ..                             # Return to the project directory
+tar czvf mychgpu_output.tgz ./OUT # Make a tarball of the output files with compression
 # Move output files to GoogleDrive shared folder. BiGdata is the shared group folder
 # BiGdata:mychgpu will create a new subfolder with the project name "mychgpu"
 rclone copy ./mychgpu_output.tgz BiGdata:mychgpu 
-rm -rf ./OUT mychgpu_output.tgz 		# Remove output files
+rm -rf ./OUT mychgpu_output.tgz   # Remove output files
